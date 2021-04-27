af5d973bee605afc817d1dbb5b76725e02fcf630
Kevin Mei <kevjinx@gmail.com>
2021-04-27 10:47:47 -0700

    phase2

---
+++ myProfile.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Your Javascript goes here
+
+  const h1Ele = document.createElement("h1")
+
+  h1Ele.setAttribute('id', 'top')
+
+  const gandhi = document.createTextNode('Mahatma Gandhi')
+
+  h1Ele.appendChild(gandhi);
+
+  document.body.appendChild(h1Ele)
+
+  console.log('“This is my profile page!”')
+
+});
+
+console.log('“This is my profile page!”')
